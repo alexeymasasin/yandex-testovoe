f975ff44675ba632db54a6952b4c46ca66573c4d
Alexey Masasin <alexeymasasin@gmail.com>
2024-12-16 11:06:17 +0500

    Desktop layout finished

---
+++ index.js
@@ -0,0 +1,3 @@
+const currentPage = document.querySelector('.slider__pages_current');
+const rightArrow = document.querySelector('.right-arrow');
+const leftArrow = document.querySelector('.left-arrow');
